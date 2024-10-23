4a25fd79dcd816f3c8984cf7c77242249670b98f
Skkings001 <skpgamer001@outlook.com>
2024-10-23 20:04:28 +0530

    Update mpd.php

--- api/mpd.php
+++ api/mpd.php
@@ -5,7 +5,7 @@ $id = isset($_GET['id']) ? $_GET['id'] : '';
 // Check if id is not empty, then proceed with the redirection
 if (!empty($id)) {
     // Redirect to the new URL with the dynamic id
-    header("Location: https://joker.cyberxstudios.tech/tataplay/$id/manifest.mpd");
+    header("Location: https://tp.babel-in.xyz/zta/$id.mpd?key=a54894666f82584778071fbc1721b3b5");
     exit;
 } else {
     // If no id is provided, you can handle the error or redirect to a default page
